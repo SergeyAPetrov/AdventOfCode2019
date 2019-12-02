3cf21bbebe4a6ecbb97a88259da34e0bd4b29a14
SergeyAPetrov <igrey@inbox.ru>
2019-12-02 20:24:26 +0000

    Day2 refactor

--- src/Day2.hs
+++ src/Day2.hs
@@ -3,44 +3,39 @@ module Day2 where
 import Lib
 import Data.List
 
-solve1 content = 
-    step xs 0
-    where 
-        ws = wordsWhen (==',') content
-        xs = map read ws
+parse s =
+    map read (wordsWhen (==',') s)
 
-solve2 content = 
+solve1 content =
+    head $ Day2.traverse xs
+    where
+        xs = parse content
+
+solve2 content =
     100*(match !! 1) + (match !! 2)
-    where 
-        ws = wordsWhen (==',') content
-        xs = map read ws
-        xss = [(put (put xs a 1) b 2) 
-                | a <- [0..99],
-                  b <- [0..99]]
-        ass = map (\list -> step2 list 0) xss 
-        matches = filter (\list->(head list)==19690720) ass
-        match = head matches
+    where
+        xs = parse content
+        possibleSolutions = [put a 1 $ put b 2 xs
+                | a <- [0..99],b <- [0..99]]
+        ass = map Day2.traverse possibleSolutions
+        match = head $ filter (\(a:as)->a==19690720) ass
 
-put xs x idx = take idx xs ++ [x] ++ drop (idx+1) xs
+put x n xs = take n xs ++ [x] ++ drop (n+1) xs
+
+traverse xs = step xs 0
 
 step xs idx =
     if xs !! idx == 99 then
-        xs !! 0
+        xs
     else
         step (step' xs idx) (idx+4)
 
-step2 xs idx =
-    if xs !! idx == 99 then
-        xs
-    else
-        step2 (step' xs idx) (idx+4)
-
-step' xs idx = 
-    put xs newValue newInd
-    where 
+step' xs idx =
+    put newValue newIdx xs
+    where
         value1 = xs !! (xs !! (idx+1))
         value2 = xs !! (xs !! (idx+2))
-        newInd = xs !! (idx+3)
+        newIdx = xs !! (idx+3)
         op = case xs !! idx of
                 1 -> (+)
                 2 -> (*)
