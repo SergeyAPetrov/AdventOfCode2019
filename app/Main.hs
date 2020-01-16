6ae2c7dddfe4094901a3cf8d9965c21af39c9e79
Sergey Petrov <igrey@inbox.ru>
2020-01-16 11:20:12 +0300

    Day16 part1

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
-import Day14
+import Day16
 
 main :: IO ()
 main = 
     do 
-        contents <- lines <$> readFile "input/14.txt"
-        --contents <- readFile "input/8.txt"
+        -- contents <- lines <$> readFile "input/14.txt"
+        contents <- readFile "input/16.txt"
         let answer = solve contents
               
         print answer 
--- src/Day16.hs
+++ src/Day16.hs
@@ -1,8 +1,29 @@
 module Day16 where
 
+import Data.Char(digitToInt)
+
+--parse :: [Char] -> [Integer]
+parse str = 
+    map digitToInt str
+
 -- n = 1..
 pattern n =
     tail $ concat $ repeat nthPattern
     where 
         basePattern = [0,1,0,-1]
         nthPattern = concatMap ((take n).repeat) basePattern
+
+--phase :: [Integer] -> [Integer]
+phase xs = 
+    map (uncurry calculateElement) $ map ((,) xs) $ map pattern [1..(length xs)] 
+
+calculateElement xs pattern = 
+    (abs total) `mod` 10
+    where 
+        total = sum $ map (uncurry (*)) $ zip xs pattern
+
+solve input = 
+    take 8 $ phases!!100
+    where
+        inputSignal = parse input
+        phases = iterate phase inputSignal
